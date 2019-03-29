189c0f4f338cb536d2dab147f5f0b7b6e28ea8bd
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-03-29 10:23:38 -0400

    created project

---
+++ src/main/java/com/zipcoder/io/SpringLearnerLab/SpringLearnerLabApplication.java
@@ -0,0 +1,13 @@
+package com.zipcoder.io.SpringLearnerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnerLabApplication.class, args);
+	}
+
+}
